e666f95926b057550fe3163b345ceb808205ad0c
SAHIDUL-ISLAM <sahidul99622@gmail.com>
2026-01-30 23:22:36 +0600

    add task5

---
+++ task5.js
@@ -0,0 +1,15 @@
+// Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+// Input: [1, 2, 3]
+
+// Expected Output:
+
+// Original: [1, 2, 3] Copy: [99, 2, 3]
+
+let input = [1,2,3];
+let copy = [...input];
+
+copy[0]=99;
+
+console.log(input);
+console.log(copy);
